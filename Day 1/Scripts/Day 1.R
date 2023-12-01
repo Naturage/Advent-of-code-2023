08ca43663615a9d9c485d60b5e9304ffff23d89a
Naturage <54602845+Naturage@users.noreply.github.com>
2023-12-01 09:52:23 +0000

    Day 1

---
+++ Day 1/Scripts/Day 1.R
@@ -0,0 +1,46 @@
+source(here::here("common functions.R"))
+library(stringr)
+
+day <- 1
+
+# Data input
+input <- get_input(day)
+
+# Pt1
+
+reg_of_interest <- "[0-9]"
+
+(ans_1 <- input %>% 
+  sapply(function(line){
+    line %>%
+      str_extract_all(reg_of_interest) %>% 
+        unlist() %>% 
+        .[c(1,length(.))] %>% 
+        paste0(collapse = "") %>% 
+        as.numeric()
+}) %>%
+  sum())
+
+# Pt2
+
+# Needs lookahead because filthy elveses read "eightwo" as 82.
+reg_of_interest_2 <- "[0-9]|(on(?=e))|(tw(?=o))|(thre(?=e))|(four)|(fiv(?=e))|(six)|(seve(?=n))|(eigh(?=t))|(nin(?=e))"
+
+decode <- list("1" = 1, "2" = 2, "3" = 3, "4" = 4, "5" = 5, 
+               "6" = 6, "7" = 7, "8" = 8, "9" = 9, "0" = 0,
+               on = 1, tw = 2, thre = 3, four = 4, fiv = 5,
+               six = 6, seve = 7, eigh = 8, nin = 9)
+
+(ans_2 <- input %>% 
+    sapply(function(line){
+      
+      line %>% 
+        str_extract_all(reg_of_interest_2) %>% 
+        unlist() %>%
+        decode[.] %>%
+        unlist %>% 
+        .[c(1,length(.))] %>% 
+        paste0(collapse = "") %>% 
+        as.numeric()
+    }) %>%
+    sum())
---
+++ common functions.R
@@ -0,0 +1,34 @@
+library(here)
+library(dplyr)
+library(httr)
+library(readr)
+library(tidyr)
+library(stringr)
+library(stringi)
+
+get_input <- function(day){
+  
+  if(file.exists(here(paste0("Day ",day), "Data","input.rds"))){
+    input <- read_rds(here(paste0("day ",day), "data","input.rds"))
+  } else {
+    suppressWarnings(session_id <- readLines(here("session.txt")))
+    
+    input <- GET(paste0("https://adventofcode.com/2023/day/",day,"/input"),
+                 set_cookies(session = session_id)) %>%
+      content(encoding = 'UTF-8') %>%
+      read_lines()
+    
+    write_rds(input, here(paste0("day ",day), "data","input.rds"))
+  }
+  
+  return(input)
+}
+
+strtoi_full <- function(binary_num, base = 2){
+  if (binary_num == ""){
+    return(0)
+  } else if (nchar(binary_num) < 30){
+    return(strtoi(binary_num, base = 2))
+  } else
+    return(strtoi_full(str_sub(binary_num, end = -31)) * 2**30 + strtoi(str_sub(binary_num, start = -30), base = 2))
+}
